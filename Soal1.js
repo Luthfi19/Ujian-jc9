e9756a5324fdb694d70888257882c05cb5a0bc76
luthfi <muhammad.luthfi1910@gmail.com>
2019-05-21 14:44:49 +0700

    Ujian

---
+++ Soal1.js
@@ -0,0 +1,9 @@
+function getMiddle(huruf){
+var hurufAlpha = huruf.split('')// ['t','e','s','t']
+var hasil = ''
+for(var i = 0 ; i < huruf.length ; i++){
+    hasil += huruf[0]
+   return hasil
+}
+
+console.log(getMiddle('Test'))// menghasilkan es
